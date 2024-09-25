3ef9acafc04aafcab41d1086312ba5c91dbb6e22
amirhosseine79 <amirhosseine2013@gmail.com>
2024-09-25 14:10:06 +0330

    dm-004 consul

--- discount/src/main/java/ir/cactus/DiscountApplication.java
+++ discount/src/main/java/ir/cactus/DiscountApplication.java
@@ -2,8 +2,10 @@ package ir.cactus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DiscountApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscountApplication.class, args);
