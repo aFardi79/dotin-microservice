085253e20acbe8754632f230cef1775958b18af9
amirhosseine79 <amirhosseine2013@gmail.com>
2024-10-02 08:07:14 +0330

    dm-004 notification application

--- discount/src/main/java/ir/cactus/controller/DiscountController.java
+++ discount/src/main/java/ir/cactus/controller/DiscountController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/discount")
+@RequestMapping("/api/v1/discount")
 public class DiscountController {
 
     @Autowired
